b8f0e5fa12757ef64d2855ceed322d6539ce44ee
Nick Felicelli <nick.felicelli@gmail.com>
2016-12-09 14:00:36 -0500

    Fixing tests

--- app/states/post-comment.js
+++ app/states/post-comment.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 const PostCommentStateObject = Ember.Object.extend();
 
 PostCommentStateObject.reopenClass({
-  initialState(instance) {
+  initialState() {
     return {
       text: null
     };
